a0cebba397d9b43a368210c869fa52e7c3f4df30
gurmark4 <91353064+gurmark4@users.noreply.github.com>
2022-09-21 03:02:18 +0300

    Update InvertedIndex.cpp

--- InvertedIndex.cpp
+++ InvertedIndex.cpp
@@ -1,46 +1,44 @@
 
 #include "search_engine.h"
 #include "InvertedIndex.h"
+#include <chrono>
 using namespace std;
 
 std::mutex freq_dictionary_mtx;
 std::mutex single_word_mtx;
 
 void thdocs(map<string, vector<Entry>>& freq_dictionary,
-	const string& iss,
-	const size_t i)
+	const std::string& ss,
+	const int m)
 {
-	std::stringstream text_of_file;
 	std::string single_word;
 	Entry ee{ 0 };
 	vector<Entry> vee;
-	ee.doc_id = i;
+	ee.doc_id = m;
 	ee.count = 1;
 	vee.clear();
 	vee.push_back(ee);
-	text_of_file.str(iss);
+	std::istringstream text_of_file(ss, std::istringstream::in);
 	while (text_of_file >> single_word)
 	{
 		single_word_mtx.lock();
 		int k1 = -1;
 		for (map<string, vector<Entry>>::iterator it = freq_dictionary.begin(); it != freq_dictionary.end(); ++it) {
 			if (single_word == it->first) {
+				freq_dictionary_mtx.lock();
 				k1 = 1;
 				int k2 = -1;
 				for (size_t j = 0; j < it->second.size(); j++) {
-					if (i == it->second[j].doc_id) {
+					if (m == it->second[j].doc_id) {
 						k2 = 1;
-						freq_dictionary_mtx.lock();
 						it->second[j].count++;
-						freq_dictionary_mtx.unlock();
 						break;
 					}
 				}
 				if (k2 < 0) {
-					freq_dictionary_mtx.lock();
 					it->second.push_back(ee);
-					freq_dictionary_mtx.unlock();
 				}
+				freq_dictionary_mtx.unlock();
 				break;
 			}
 		}
@@ -59,6 +57,7 @@ void thdocs(map<string, vector<Entry>>& freq_dictionary,
 		for (size_t i = 0; i < input_docs.size(); i++) {
 			std::istringstream iss(input_docs[i], std::istringstream::in);
 			fls.push_back(thread(thdocs, ref(freq_dictionary), iss.str(), i));
+				this_thread::sleep_for(chrono::milliseconds(100));
 		}
 		for (size_t i = 0; i < input_docs.size(); i++) if (fls[i].joinable()) fls[i].join();
 	};
